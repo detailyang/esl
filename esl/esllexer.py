d96c872b89eccee96dc691071d05dbd9897605b6
detailyang <detailyang@gmail.com>
2016-03-30 21:04:56 +0800

    release to pip

--- esl/esllexer.py
+++ esl/esllexer.py
@@ -1,9 +1,23 @@
+# @Author: BingWu Yang <detailyang>
+# @Date:   2016-03-29T17:46:26+08:00
+# @Email:  detailyang@gmail.com
+# @Last modified by:   detailyang
+# @Last modified time: 2016-03-30T20:51:25+08:00
+# @License: The MIT License (MIT)
+
+
+from ply.lex import TOKEN
+import re
 import ply.lex as lex
 
 
 digit            = r'([0-9])'
 nondigit         = r'([_A-Za-z])'
 variable         = r'[a-zA-Z0-9\[\]-_]+'
+header           = r'--h' + variable + r'=' + variable
+querystring = r'--q' + variable + r'=' + variable
+body = r'--b' + variable + r'=' + variable
+
 
 class ESLLexer:
     # List of token names.   This is always required
@@ -15,13 +29,30 @@ class ESLLexer:
        'BODY'
     )
 
-
     # Regular expression rules for simple tokens
-    t_URL = r'^[^ ]+'
-    t_METHOD = r'(GET|get|POST|post|DELETE|delete|OPTIONS|options|CONNECT|connect)'
-    t_HEADER = r'--h' + variable + r'=' + variable
-    t_QUERYSTRING = r'--q' + variable + r'=' + variable
-    t_BODY = r'--b' + variable + r'=' + variable
+    # t_HEADER = r'--h' + variable + r'=' + variable
+    # t_QUERYSTRING = r'--q' + variable + r'=' + variable
+    # t_BODY = r'--b' + variable + r'=' + variable
+    def t_URL(self, t):
+        r'^[^ ]+'
+        return t
+
+    def t_METHOD(self, t):
+        r'(GET|get|POST|post|DELETE|delete|OPTIONS|options|CONNECT|connect)'
+        t.value = t.value
+        return t
+
+    @TOKEN(header)
+    def t_HEADER(self, t):
+        return t
+
+    @TOKEN(querystring)
+    def t_QUERYSTRING(self, t):
+        return t
+
+    @TOKEN(body)
+    def t_BODY(self, t):
+        return t
 
     # Define a rule so we can track line numbers
     def t_newline(self,t):
@@ -47,7 +78,3 @@ class ESLLexer:
              tok = self.lexer.token()
              if not tok: break
              print tok
-
-m = ESLLexer()
-m.build()           # Build the lexer
-m.test("http://aasdfasdf.sdf.sdf get --habc=a --qdef=123a --basdf[]=sdfk")     # Test it

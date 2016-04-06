3044e5044a028bbafdb129249791e2ea1b900bbb
detailyang <detailyang@gmail.com>
2016-04-06 21:24:05 +0800

    [typo]

--- esl/eslgenerator.py
+++ esl/eslgenerator.py
@@ -2,22 +2,22 @@
 # @Date:   2016-04-06T21:18:58+08:00
 # @Email:  detailyang@gmail.com
 # @Last modified by:   detailyang
-# @Last modified time: 2016-04-06T21:20:38+08:00
+# @Last modified time: 2016-04-06T21:24:01+08:00
 # @License: The MIT License (MIT)
 
 
 class ESLGenerator(object):
     def __init__(self, ast):
-        self.ast
+        self.ast = ast
 
-    def to_curl():
+    def to_curl(self):
         pass
 
-    def to_go():
+    def to_go(self):
         pass
 
-    def to_python():
+    def to_python(self):
         pass
 
-    def to_node():
+    def to_node(self):
         pass
